0f2d0a6aaeb37cf440bc6a81e382baf9ad57fc2f
orsasson12 <orsasson72@gmail.com>
2023-01-19 13:52:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+import Features from './components/features/Features';
+import Footer from './components/footer/Footer';
+import Hero from './components/header/Hero';
+import About from './components/main/About';
+import Recommendations from './components/recommendations/Recommendations';
+import { createTheme, ThemeProvider, Box } from '@mui/material';
 function App() {
+  const [mode, setMode] = useState("light")
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode
+    }
+  })
+  Aos.init({
+    duration: 1800,
+    offset: 0
+  })
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={'text.primary'}>
+        <Hero setMode={setMode} mode={mode} />
+        <About />
+        <Features />
+        <Recommendations />
+        <Footer />
+      </Box>
+    </ThemeProvider>
+
   );
 }
 
